0f74d63826ab5de1a3ad9890f6b3271ef44f1028
Otavio Ferreira <otavio.cferreira@gmail.com>
2017-02-25 13:33:14 -0300

    Diminuindo intervalo de troca de banner

--- principal.js
+++ principal.js
@@ -1,1 +1,11 @@
-ECHO est  ativado.
+var banners = 
+	["Os melhores do Brasil!", "Qualidade e preço baixo!"];
+var bannerAtual = 0;
+
+function() {
+	bannerAtual = (bannerAtual + 1) % 2;
+	document.querySelector('h2#mensagem').textContent = 
+		banners[bannerAtual];
+}
+
+setInterval(trocaBanner, 1000);
